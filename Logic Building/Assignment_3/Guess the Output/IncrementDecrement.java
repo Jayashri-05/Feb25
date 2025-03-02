af4bb4f6191f49926bde0f8a4ca90ac730dab1a0
Jayashri-05 <jayashri2015d@gmail.com>
2025-03-02 13:29:02 +0530

    Add files via upload

---
+++ Logic Building/Assignment_3/Guess the Output/IncrementDecrement.java
@@ -0,0 +1,16 @@
+//Snippet 6: 
+public class  { 
+ public static void main(String[] args) { 
+ int x = 5; 
+ int y = ++x - x-- + --x + x++; 
+ System.out.println(y); 
+ } 
+} 
+/* Guess the output of this code snippet.
+++x ->preIncrement Operator
+x++ ->PostIncrement Operator
+--x ->preDecrement Operator
+x-- ->PostDecrement Operator
+int x=5;    
+*/
+ 
